db06c85114b84d2dd3f09eac843b131fb2b3e67f
RittaNarita <narittan@gmail.com>
2014-11-28 15:12:55 +0900

    change package name

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
-import music_learning
+import mudpy
 
 setup(
-        name             = 'music_learning',
+        name             = 'MudPy',
         # version          = __version__,
         description      = 'music analysis tools for recommendation with deep learning',
         # license          = __license__,
         # author           = __author__,
         author_email     = 'narittan@gmail.com',
-        url              = 'https://github.com/RittaNarita/music_learning',
-        keywords         = 'deep learning for music',
-        packages         = ['music_learning'],
+        url              = 'https://github.com/RittaNarita/MudPy',
+        keywords         = 'MUsic Deep learning',
+        packages         = ['mudpy'],
         install_requires = [],
         )
