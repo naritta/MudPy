7b42b5fe29bc6f300e43c4494b88a1277815b594
RittaNarita <narittan@gmail.com>
2014-11-28 15:30:36 +0900

    improve __init__ info

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
-import mudpy
+from mudpy import __version__,__license__,__author__
 
 setup(
         name             = 'MudPy',
-        # version          = __version__,
+        version          = __version__,
         description      = 'music analysis tools for recommendation with deep learning',
-        # license          = __license__,
-        # author           = __author__,
+        license          = __license__,
+        author           = __author__,
         author_email     = 'narittan@gmail.com',
         url              = 'https://github.com/RittaNarita/MudPy',
         keywords         = 'MUsic Deep learning',
